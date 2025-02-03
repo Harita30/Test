928edfaf912fed94901599086027bda0f4836cc0
haritaanbuvelan <hanbuvel@iu.edu>
2025-02-03 12:58:25 -0600

    Changes to css

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import React from "https://cdn.skypack.dev/react@17.0.1";
 import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+import "./App.css";
 
 const logoSrc =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logoSrc} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="Parts">
+        <p>Part 1</p>
+        <p>Part 2</p>
+        <p>Part 3</p>
+      </div>
     </div>
   );
 }
